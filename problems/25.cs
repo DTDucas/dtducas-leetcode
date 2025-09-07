14ec396ea82d616f8bd815735329f9624ff41bea
Duong Tran <dtducas@gmail.com>
2025-09-07 13:47:38 +0700

    ✨ feat(problems): resolved problem 25

---
+++ problems/25.cs
@@ -0,0 +1,40 @@
+// Author: Duong Tran Quang (aka DTDucas)
+// Email: baymax.contact@gmail.com
+// GitHub: https://github.com/DTDucas
+// Source: https://github.com/DTDucas/dtducas-leetcode
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) return head;
+
+        ListNode current = head;
+        for (int i = 0; i < k; i++) {
+            if (current == null) return head;
+            current = current.next;
+        }
+
+        ListNode prev = null;
+        current = head;
+        for (int i = 0; i < k; i++) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head.next = ReverseKGroup(current, k);
+
+        return prev;
+    }
+}
